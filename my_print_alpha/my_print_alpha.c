c12d44a62649cc0f800fafc779cc0304d51d9913
root <root@debian>
2020-10-19 08:58:47 -0400

    modif print alpha

---
+++ my_print_alpha/my_print_alpha.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+/*
+** ETNA PROJECT, 19/10/2020
+** Frist C Programming
+** File description:
+** 		No file there, just an etna header example
+*/
+
+void my_putchar(char c){
+  write(1, &c, 1);
+}
+
+void my_print_alpha(void){
+  char letter = 'a';
+   for(letter='a'; letter <= 'z'; letter++){
+     // printf("%c", letter); "\n";
+     my_putchar(letter);"\n";
+   }
+}
+int main(int argc, char **argv){
+    my_print_alpha();
+
+    return (0);
+}
