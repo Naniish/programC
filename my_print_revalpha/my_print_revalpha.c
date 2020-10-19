4487ddc30cf81dea0fc1df8de673a7a1fc7142f8
root <root@debian>
2020-10-19 10:07:42 -0400

    my print reverse alphabet ok

---
+++ my_print_revalpha/my_print_revalpha.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+/*
+** ETNA PROJECT, 2020
+** Frist C Programming
+** File description:
+** 		No file there, just an etna header example
+*/
+
+void my_putchar(char c){
+  write(1, &c, 1);
+}
+
+void my_print_alpha(void){
+  char letter = 'z';
+   while(letter >= 'a'){
+     my_putchar(letter);
+       letter--;
+   }
+}
+int main(int argc, char **argv){
+    my_print_alpha();
+
+    return (0);
+}
