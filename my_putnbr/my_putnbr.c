4a4a02144b7597b6008fd2ea1a24e3fcbbba729a
root <root@debian>
2020-10-20 03:46:05 -0400

    my putnbr OK

---
+++ my_putnbr/my_putnbr.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+** ETNA PROJECT, 2020
+** Frist C Programming
+** File description:
+** 		No file there, just an etna header example
+*/
+
+void my_putnbr(int n){
+  printf("%d", n);
+  
+}
